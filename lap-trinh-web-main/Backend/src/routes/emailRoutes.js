b9221d4512bfd7c5684590d5c8c631d7678aebc0
Thangbdu <24050129@student.bdu.edu.vn>
2026-06-05 16:40:21 +0700

    Delete lap-trinh-web-main directory

--- lap-trinh-web-main/Backend/src/routes/emailRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/emailController');
-
-router.post('/forgot-password', emailController.forgotPassword);
-router.post('/verify-otp', emailController.verifyOtp);
-router.post('/reset-password', emailController.resetPassword);
-
-module.exports = router;
--- lap-trinh-web-main/Backend/src/routes/oauthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const oauthController = require('../controllers/oauthController');
-
-router.post('/google', oauthController.googleLogin);
-router.post('/facebook', oauthController.facebookLogin);
-
-module.exports = router;
